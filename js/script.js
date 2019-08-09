c809348e78caedca8f98c3223851a67ffd34cbff
anna_f <annafoldvari@gmx.co.uk>
2019-08-09 15:08:19 +0100

    Project finished

--- js/script.js
+++ js/script.js
@@ -3,57 +3,11 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// Array of quote objects.
 
 var quotes = [
   {
@@ -63,7 +17,7 @@ var quotes = [
     tags: "burdens, empowerment, flying, freedom, unburdening, weight"
   },
   {
-    quote: "f you don’t like something, change it. If you can’t change it, change your attitude.",
+    quote: "If you don’t like something, change it. If you can’t change it, change your attitude.",
     source: "Maya Angelou"
   },
   {
@@ -85,7 +39,55 @@ var quotes = [
     citation: "Sapiens",
     tags: "history, sociopolitical hierarchies, myths"
   },
-
-
-
 ];
+
+// Gets a random quote object from the quotes array
+
+function getRandomQuote(quotes) {
+  var randomNumber = Math.floor(Math.random() * (quotes.length - 1) + 1);
+  return quotes[randomNumber];
+}
+
+/*Builds up the html with the details of one selected quotes and prints it out in the adequate div. 
+Also changes the background color of the body tag by calling the changeBackgroundColor function. */
+
+function printQuote() {
+  var html = '';
+  var quote = getRandomQuote(quotes);
+
+  html = `<p class="quote">${quote.quote}</p>
+          <p class="source">${quote.source}`;
+  
+  if (quote.citation) {
+    html += `<span class="citation">${quote.citation}</span>`;
+  }
+  
+  if (quote.year) {
+    html += `<span class="year">${quote.year}</span>`;
+  }
+
+  html += '</p>';
+
+  if (quote.tags) {
+    html += `<h4>Tags: ${quote.tags}</h4>`;
+  }
+
+  var quoteDiv = document.getElementById("quote-box");
+  quoteDiv.innerHTML = html;
+  changeBackgroundColor();
+}
+
+// Changes the background color of the body tag with a randomly chosen color from the colors array
+
+function changeBackgroundColor() {
+  var colors = ['#ffff00', '#ff3333', '#ff1aff', '#1a75ff', '#00ff00'];
+  var randomIndex = Math.floor(Math.random() * (colors.length - 1) + 1);
+  var randomColor = colors[randomIndex];
+
+  var myElement = document.getElementsByTagName("body");
+  myElement[0].style.backgroundColor = randomColor;
+}
+
+// Every 20 seconds calls the printQuote function, thus changing the quote and the background color.
+
+window.setInterval(printQuote, 20000);
